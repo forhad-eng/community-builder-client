afe5e7af1e457bda5e2ae1652fa0ba619a76012b
forhad-eng <forhadu.cse@gmail.com>
2022-04-27 01:27:56 +0600

    booking delete functionality added

--- src/Pages/Home/BookedActivities/BookedActivities.js
+++ src/Pages/Home/BookedActivities/BookedActivities.js
@@ -19,13 +19,34 @@ const BookedActivities = () => {
         getBookedActivity()
     }, [user])
 
+    const deleteBookedActivityHandle = async _id => {
+        const proceed = window.confirm('Are you sure you want to cancel?')
+        if (proceed) {
+            const filter = bookedActivities.filter(activity => activity._id !== _id)
+            const url = `http://localhost:5000/book/${_id}`
+            const { data } = await axios.delete(url)
+            if (data.acknowledged) {
+                setBookedActivities(filter)
+                alert('Canceled')
+            }
+        }
+    }
+
     return (
         <div className="bg-[#E5E5E5] h-screen">
-            <div className="grid lg:grid-cols-2 gap-x-10 gap-y-5 px-20 py-10 ">
-                {bookedActivities.map(bookedActivity => (
-                    <BookedActivity key={bookedActivity._id} bookedActivity={bookedActivity} />
-                ))}
-            </div>
+            {bookedActivities.length === 0 ? (
+                <p className="text-center text-3xl font-[500] py-28">No Booking Yet!</p>
+            ) : (
+                <div className="grid lg:grid-cols-2 gap-x-10 gap-y-5 px-20 py-10 ">
+                    {bookedActivities.map(bookedActivity => (
+                        <BookedActivity
+                            key={bookedActivity._id}
+                            bookedActivity={bookedActivity}
+                            deleteActivity={deleteBookedActivityHandle}
+                        />
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
--- src/Pages/Home/BookedActivity/BookedActivity.js
+++ src/Pages/Home/BookedActivity/BookedActivity.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const BookedActivity = ({ bookedActivity }) => {
-    const { activity, date, picture } = bookedActivity
+const BookedActivity = ({ bookedActivity, deleteActivity }) => {
+    const { activity, date, picture, _id } = bookedActivity
     return (
         <div className="flex gap-5 shadow-lg rounded-md bg-white p-4 relative">
             <div className="flex gap-8">
@@ -11,7 +11,10 @@ const BookedActivity = ({ bookedActivity }) => {
                     <p className="text-lg font-[500]">{date}</p>
                 </div>
             </div>
-            <button className="bg-[#E3E3E3] text-[#787878] px-5 py-2 rounded-md absolute right-5 bottom-4">
+            <button
+                onClick={() => deleteActivity(_id)}
+                className="bg-[#E3E3E3] text-[#787878] px-5 py-2 rounded-md absolute right-5 bottom-4"
+            >
                 Cancel
             </button>
         </div>
